05556aa6776a9f1772aa9e2536f9a77c9cc8c384
smaranika05 <41224686+smaranika05@users.noreply.github.com>
2020-12-01 20:03:43 +0530

    Update Address.js

--- src/views/address/Address.js
+++ src/views/address/Address.js
@@ -1,21 +1,58 @@
 import React, { Component } from "react";
+​
 import building from "./../../assets/img/building.jpg";
+​
 import Footer from "./../../components/footer/footer";
+​
 import "./Address.scss";
 import "./../../views/Heading.scss";
+​
 class Address extends Component {
   componentDidMount = () => {
     window.scrollTo(0, 0);
   };
   render() {
     return (
-      <div className="kont_wrap">
-        
-      
+      <div className="addr">
+        <div className="addr_wrapper">
+          <div className="header"> HIER FINDEN SIE UNS </div>
+          <div className="border"></div>
+          <div className="top_wrapper">
+            <div className="descNimg">
+              <p style={{ color: "#5ECD73" }}>Praxisbezeichnung</p>
+              <p>Praxis Dr. Dettner</p>
+              <br></br>
+              <p style={{ color: "#5ECD73" }}>Name</p>
+              <p>Herr Dr. med. Ottmar Dettner</p>
+              <br></br>
+              <p style={{ color: "#5ECD73" }}>Facharztbezeichnung</p>
+              <p>Facharzt für Allgemeinmedizin</p>
+              <br></br>
+              <p style={{ color: "#5ECD73" }}>Adresse</p>
+              <p> Kissinger Str. 42</p>
+              <p>97762 Hammelburg</p>
+              <br></br>
+              <p>1. Stock - Treppenaufgang</p>
+              <br></br>
+              <p style={{ color: "#5ECD73" }}>Telefon</p>
+              <p> 09732-1247</p>
+              <br></br>
+              <p style={{ color: "#5ECD73" }}>Telefax</p>
+              <p> 09732-1869</p>
+              <br></br>
+              <p style={{ color: "#5ECD73" }}>E-Mail-Adresse</p>
+              <p>dr.dettner@gmail.com</p>
+            </div>
+            <div className="building" name-member="building">
+              <img src={building} alt="building" />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
 }
+​
 export default Address;
 
 
