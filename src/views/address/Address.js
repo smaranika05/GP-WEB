50513f1f7c17b3d8a30b5decf136b192ac3cb937
smaranika05 <41224686+smaranika05@users.noreply.github.com>
2020-12-01 20:09:19 +0530

    Update Address.js

--- src/views/address/Address.js
+++ src/views/address/Address.js
@@ -1,12 +1,8 @@
 import React, { Component } from "react";
-​
 import building from "./../../assets/img/building.jpg";
-​
 import Footer from "./../../components/footer/footer";
-​
 import "./Address.scss";
 import "./../../views/Heading.scss";
-​
 class Address extends Component {
   componentDidMount = () => {
     window.scrollTo(0, 0);
